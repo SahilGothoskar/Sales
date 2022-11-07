3e261588fd1fbd174920bfb7b57e38de573f40a9
Sahil Gothoskar <33109078+SahilGothoskar@users.noreply.github.com>
2022-11-07 18:52:25 -0500

    Add files via upload

--- Twitter_Bot/SQL_Extraction_From_CSV_Load_DB.py
+++ Twitter_Bot/SQL_Extraction_From_CSV_Load_DB.py
@@ -10,36 +10,45 @@ connection = sqlite3.connect('Tweets_Extracted.db')
 cursor = connection.cursor()
 
 # Table Definition
-create_table = '''CREATE TABLE person(
+create_table = '''CREATE TABLE tweets(
 				id INTEGER PRIMARY KEY AUTOINCREMENT,
-				name TEXT NOT NULL,
-				age INTEGER NOT NULL);
+				username VARCHAR NOT NULL,
+				description VARCHAR NOT NULL,
+				location VARCHAR NOT NULL,
+				following INTEGER NOT NULL,
+				followers INTEGER NOT NULL,
+				totaltweets INTEGER NOT NULL,
+				retweetcount INTEGER NOT NULL,
+				text VARCHAR NOT NULL,
+				hastags VARCHAR NOT NULL
+				);
 				'''
 
 # Creating the table into our
 # database
 cursor.execute(create_table)
 
-# Opening the person-records.csv file
-file = open('GFG_tweets.csv')
+# Opening the tweets-records.csv file
+file = open('GFG_tweets.csv' , errors='ignore')
 
 # Reading the contents of the
-# person-records.csv file
+# tweets-records.csv file
 contents = csv.reader(file)
 
+
 # SQL query to insert data into the
-# person table
-insert_records = "INSERT INTO person (name, age) VALUES(?, ?)"
+# tweets table
+insert_records = "INSERT INTO tweets (id,username,description,location,following,followers,totaltweets,retweetcount,text,hastags) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"
 
 # Importing the contents of the file
-# into our person table
+# into our tweets table
 cursor.executemany(insert_records, contents)
 
 # SQL query to retrieve all data from
-# the person table To verify that the
+# the tweets table To verify that the
 # data of the csv file has been successfully
 # inserted into the table
-select_all = "SELECT * FROM person"
+select_all = "SELECT * FROM tweets"
 rows = cursor.execute(select_all).fetchall()
 
 # Output to the console screen
