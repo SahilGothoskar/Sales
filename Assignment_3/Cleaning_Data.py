48b76f759a30b785c0b141e5d88a1a1228c71084
Sahil Gothoskar <33109078+SahilGothoskar@users.noreply.github.com>
2022-11-26 18:53:38 -0500

    Update Cleaning_Data.py

--- Assignment_3/Cleaning_Data.py
+++ Assignment_3/Cleaning_Data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
 
-walmart_path = 'D:\DMDD\Assignment 3\Walmart.csv'
+walmart_path = 'Walmart.csv'
 
 walmart_ori = pd.read_csv(walmart_path)
 walmart = walmart_ori.copy()
