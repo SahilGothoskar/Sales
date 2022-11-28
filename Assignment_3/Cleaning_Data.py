169a4397631f7ca8df91a16989b558f1a5e0d041
Sahil Gothoskar <33109078+SahilGothoskar@users.noreply.github.com>
2022-11-28 14:04:05 -0500

    Code Changes

--- Assignment_3/Cleaning_Data.py
+++ Assignment_3/Cleaning_Data.py
@@ -3,6 +3,7 @@ import numpy as np
 
 #import matplotlib.pyplot as plt
 
+#Dataset 1
 walmart_path = 'D:\DMDD\Assignment 3\myCity.csv'
 
 walmart_ori = pd.read_csv(walmart_path)
@@ -12,7 +13,7 @@ walmart.head()
 
 walmart.info()
 
-
+#Function to fetch missing values from Dataset 1
 def missing_cols(walmart):
     '''prints out columns with its amount of missing values'''
     total = 0
@@ -48,3 +49,61 @@ missing_cols(walmart)
 
 #Percentage Wise Missing Values
 perc_missing(walmart)
+
+
+# imputing with bfill or ffill
+walmart['Metro'].bfill(inplace=True)
+walmart['Metro'].ffill(inplace=True)
+walmart['SizeRank'].bfill(inplace=True)
+walmart['SizeRank'].ffill(inplace=True)
+walmart['SellForGain'].bfill(inplace=True)
+walmart['SellForGain'].ffill(inplace=True)
+walmart['ZHVI'].bfill(inplace=True)
+walmart['ZHVI'].ffill(inplace=True)
+walmart['MoM'].bfill(inplace=True)
+walmart['MoM'].ffill(inplace=True)
+walmart['ForecastYoYPctChange'].ffill(inplace=True)
+walmart['ForecastYoYPctChange'].bfill(inplace=True)
+walmart['YoY'].ffill(inplace=True)
+walmart['YoY'].ffill(inplace=True)
+walmart['Delinquency'].ffill(inplace=True)
+walmart['Delinquency'].ffill(inplace=True)
+walmart['DaysOnMarket'].ffill(inplace=True)
+walmart['DaysOnMarket'].ffill(inplace=True)
+walmart['NegativeEquity'].ffill(inplace=True)
+walmart['NegativeEquity'].ffill(inplace=True)
+
+
+missing_cols(walmart)
+#Cleaned DF 1
+print (walmart)
+
+#Cleaned Data Inserted into CSV
+csv_data = walmart.to_csv('D:\DMDD\Assignment 3\Cleaned.csv', index = True)
+
+
+#Dataset 2
+walmart_path_1 = 'D:\DMDD\Assignment 3\Walmart.csv'
+
+walmart_ori_1 = pd.read_csv(walmart_path_1)
+walmart_1 = walmart_ori_1.copy()
+
+walmart_1.head()
+
+walmart_1.info()
+
+#Function to fetch missing values from Dataset 1
+def missing_cols(walmart_1):
+    '''prints out columns with its amount of missing values'''
+    total = 0
+    for col in walmart_1.columns:
+        missing_vals = walmart_1[col].isnull().sum()
+        total += missing_vals
+        if missing_vals != 0:
+            print(f"{col} => {walmart_1[col].isnull().sum()}")
+    
+    if total == 0:
+        print("no missing values left")
+
+missing_cols(walmart_1)
+            
